6693fc95240174d8ec1a2ad66ed52ad969eef11b
Allan <47871461+Holy-Hero@users.noreply.github.com>
2023-01-09 22:50:02 -0700

    Added the ability to print it's own source code

--- Lab01Script.py
+++ Lab01Script.py
@@ -1,11 +1,16 @@
+"""
+This is the source code
+"""
 # a python script to get the version of the requests library
 # also curls the Google homepage
+# and prints the source code
 import requests
 
 
 def main():
     print(f"The version of requests library is {requests.__version__}")
     print(requests.get('http://google.com/'))
+    print(*open(__file__), sep="")
 
 
 if __name__ == "__main__":
