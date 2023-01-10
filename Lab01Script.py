15007d00acfafab15d21b2c0aa48f39735a97c8d
Allan <47871461+Holy-Hero@users.noreply.github.com>
2023-01-09 22:45:05 -0700

    Added the curl to the script

--- Lab01Script.py
+++ Lab01Script.py
@@ -1,9 +1,11 @@
 # a python script to get the version of the requests library
+# also curls the Google homepage
 import requests
 
 
 def main():
     print(f"The version of requests library is {requests.__version__}")
+    print(requests.get('http://google.com/'))
 
 
 if __name__ == "__main__":
