7e1a1f5da694e541abb15cc07a7522151e871037
Allan <47871461+Holy-Hero@users.noreply.github.com>
2023-01-11 10:47:39 -0700

    modified the link

--- Lab01Script.py
+++ Lab01Script.py
@@ -9,8 +9,10 @@ import requests
 
 def main():
     print(f"The version of requests library is {requests.__version__}")
-    print(requests.get('http://google.com/'))
-    print(*open(__file__), sep="")
+    print(requests.get("http://google.com/"))
+
+    file = requests.get("https://raw.githubusercontent.com/Holy-Hero/CMPUT-404-LAB01/main/Lab01Script.py")
+    print(file.text)
 
 
 if __name__ == "__main__":
